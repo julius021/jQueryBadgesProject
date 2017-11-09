77954a71f4b1721d23d5826407dac6fec190c7f1
LACARELLE Julien <julien.lacarelle@orange.com>
2017-11-09 17:04:13 +0100

    push

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,13 @@
 $(function() {
 
   // your code will go here
-
+  $.ajax({
+    url: "https://www.codeschool.com/users/4346082",
+    contentType: "jsonp",
+    success: function(response){
+      response.courses.completed.forEach(element => {
+        $("#badges").append('<div class="course"><h3>'+ element.title +'</h3><img src="' + element.badge + '" /><a href="'+ element.url +'" target="_blank" class="btn btn-primary"></a></div>');
+      });
+    }
+  });
 });
