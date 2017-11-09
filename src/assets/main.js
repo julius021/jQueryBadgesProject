302d9f1d2512738eddabef58db0bb1f8888cab2a
LACARELLE Julien <julien.lacarelle@orange.com>
2017-11-09 17:14:54 +0100

    test

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,7 +3,7 @@ $(function() {
   // your code will go here
   $.ajax({
     url: "https://www.codeschool.com/users/4346082.json",
-    contentType: "jsonp",
+    dataType: "jsonp",
     success: function(response){
       response.courses.completed.forEach(element => {
         $("#badges").append('<div class="course"><h3>'+ element.title +'</h3><img src="' + element.badge + '" /><a href="'+ element.url +'" target="_blank" class="btn btn-primary"></a></div>');
